ac4de0463f25ebc8fb4262e462369dd138659423
p0ryae <contact@porya.me>
2026-04-16 14:22:07 -0700

    add the rest of lsp and formatters for nvim

--- home/nvim.nix
+++ home/nvim.nix
@@ -3,15 +3,22 @@
   enable = true;
   withNodeJs = true;
   extraPackages = with pkgs; [
-    stylua
     typescript-go
     nixd
-    nixfmt
     gopls
     svelte-language-server
     clang-tools
     neocmakelsp
     pyright
+    elixir-ls
+    jdt-language-server
+
+    stylua
+    nixfmt
+    prettierd
+    google-java-format
+    sql-formatter
+    black
   ];
   plugins = with pkgs.vimPlugins; [
     nvim-treesitter.withAllGrammars
