9e4d7a02b7e92fedb52650bd8909c49e352f64e7
p0ryae <contact@porya.me>
2026-07-19 16:51:00 -0700

    update cpuid_fault_emulation

--- hosts/b550f/cpuid_fault_emulation/cpuid_fault_emulation.nix
+++ hosts/b550f/cpuid_fault_emulation/cpuid_fault_emulation.nix
@@ -2,25 +2,36 @@
   lib,
   stdenv,
   kernel,
+  clang,
+  lld,
 }:
-
 stdenv.mkDerivation {
   pname = "cpuid_fault_emulation";
   version = "0.1";
-
   src = ./.;
 
-  nativeBuildInputs = kernel.moduleBuildDependencies;
+  nativeBuildInputs = kernel.moduleBuildDependencies ++ [
+    clang
+    lld
+  ];
 
   makeFlags = [
     "KERNELRELEASE=${kernel.modDirVersion}"
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "CC=clang"
+    "LLVM=1"
+    "KCFLAGS=-Wno-unused-command-line-argument"
+    "KAFLAGS=-Wno-unused-command-line-argument"
   ];
 
   buildPhase = ''
     make \
       -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build \
       M=$PWD \
+      CC=clang \
+      LLVM=1 \
+      KCFLAGS=-Wno-unused-command-line-argument \
+      KAFLAGS=-Wno-unused-command-line-argument \
       modules
   '';
 
